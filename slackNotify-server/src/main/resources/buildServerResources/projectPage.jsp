e9d8420cfc8d364d4f240169144fb3c01c865278
akvak <akvak@localhost>
2017-06-14 01:15:45 +0300

    Blank views for build and project pages

---
+++ slackNotify-server/src/main/resources/buildServerResources/projectPage.jsp
@@ -0,0 +1,3 @@
+<%@ include file="/include.jsp" %>
+
+<h1>Hello there! I am a project</h1>
--- slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/pages/BuildPage.scala
+++ slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/pages/BuildPage.scala
@@ -6,19 +6,19 @@ import javax.servlet.http.HttpServletRequest
 import com.fpd.teamcity.slackNotify.Strings
 import jetbrains.buildServer.serverSide.{ProjectManager, SBuildType}
 import jetbrains.buildServer.users.SUser
-import jetbrains.buildServer.web.openapi.WebControllerManager
+import jetbrains.buildServer.web.openapi.{PluginDescriptor, WebControllerManager}
 import jetbrains.buildServer.web.openapi.buildType.BuildTypeTab
 
-class BuildPage(manager: WebControllerManager, projectManager: ProjectManager) extends BuildTypeTab(
+class BuildPage(manager: WebControllerManager, projectManager: ProjectManager, descriptor: PluginDescriptor) extends BuildTypeTab(
   Strings.tabId,
   Strings.label,
   manager: WebControllerManager,
   projectManager: ProjectManager,
-  BuildPage.includeUrl) {
+  descriptor.getPluginResourcesPath(BuildPage.includeUrl)) {
 
   override def fillModel(model: util.Map[String, AnyRef], request: HttpServletRequest, buildType: SBuildType, user: SUser): Unit = ???
 }
 
 object BuildPage {
-  private def includeUrl: String = "/buildPage.jsp"
+  private def includeUrl: String = "buildPage.jsp"
 }
