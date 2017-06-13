eb3596079a17eb3ca106a9ffcca94c4196a2cf26
Alex Kvak <1052418+alexkvak@users.noreply.github.com>
2017-06-14 01:01:18 +0300

    Channel removed from global config. Strings object introduced.

--- slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/SlackNotifier.scala
+++ slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/SlackNotifier.scala
@@ -4,16 +4,32 @@ import java.util
 
 import jetbrains.buildServer.notification.{NotificatorAdapter, NotificatorRegistry}
 import jetbrains.buildServer.serverSide.SRunningBuild
-import jetbrains.buildServer.users.SUser
+import jetbrains.buildServer.users.{NotificatorPropertyKey, SUser}
+
+import scala.language.implicitConversions
 
 class SlackNotifier(notificatorRegistry: NotificatorRegistry, gateway: SlackGateway) extends NotificatorAdapter {
+  import SlackNotifier._
 
   notificatorRegistry.register(this)
 
-  override def notifyBuildStarted(build: SRunningBuild, users: util.Set[SUser]): Unit =
-    gateway.sendMessage(s"${build.getAgentName} started")
+  override def notifyBuildStarted(build: SRunningBuild, users: util.Set[SUser]): Unit = users.forEach { user ⇒
+    Option(user.getPropertyValue(channelPropertyKey)).foreach { channel ⇒
+      gateway.sendMessage(channel, s"${build.getAgentName} started")
+    }
+  }
+  override def getNotificatorType: String = notificatorType
 
-  override def getNotificatorType: String = "slackNotifier"
+  override def getDisplayName: String = displayName
+}
 
-  override def getDisplayName: String = "Slack Notifier"
+object SlackNotifier {
+  implicit private def propertyNameToPropertyKey(propertyName: String): NotificatorPropertyKey =
+    new NotificatorPropertyKey(notificatorType, propertyName)
+
+  private def notificatorType: String = Strings.tabId
+
+  private def displayName: String = Strings.label
+
+  private def channelPropertyKey: String = "ChannelName"
 }
---
+++ slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/Strings.scala
@@ -0,0 +1,6 @@
+package com.fpd.teamcity.slackNotify
+
+object Strings {
+  def label: String = "Slack Notifier"
+  def tabId: String = "SlackNotifier"
+}
