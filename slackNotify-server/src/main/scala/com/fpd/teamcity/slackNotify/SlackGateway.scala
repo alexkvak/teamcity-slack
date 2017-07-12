45b5ba31e456635dcf18117d3f35fd9f65598b19
Alex Kvak <1052418+alexkvak@users.noreply.github.com>
2017-07-12 16:03:10 +0300

    Send messages to committees if build fails

--- slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/SlackGateway.scala
+++ slackNotify-server/src/main/scala/com/fpd/teamcity/slackNotify/SlackGateway.scala
@@ -1,30 +1,47 @@
 package com.fpd.teamcity.slackNotify
 
-import com.ullink.slack.simpleslackapi.{SlackChannel, SlackMessageHandle, SlackSession}
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
 import com.ullink.slack.simpleslackapi.replies.SlackMessageReply
+import com.ullink.slack.simpleslackapi.{SlackAttachment, SlackMessageHandle, SlackSession}
 
+import scala.language.implicitConversions
 import scala.util.Try
 
 object SlackGateway {
-  case class Destination(session: SlackSession, channel: SlackChannel)
+
+  sealed trait Destination
+
+  case class SlackUser(email: String) extends Destination
+
+  case class SlackChannel(name: String) extends Destination
+
+  case class SlackMessage(message: String, attachment: Option[SlackAttachment] = None)
+
+  implicit def stringToSlackMessage(message: String): SlackMessage = SlackMessage(message)
 
   type MessageSent = SlackMessageHandle[SlackMessageReply]
 
   def sessionByConfig(config: ConfigManager.Config): Option[SlackSession] = {
+    // TODO: cache connection
     val session = SlackSessionFactory.createWebSocketSlackSession(config.oauthKey)
     Try(session.connect()).map(_ ⇒ session).toOption
   }
 }
 
 class SlackGateway(val configManager: ConfigManager) {
+
   import SlackGateway._
 
   def session: Option[SlackSession] = configManager.config.flatMap(sessionByConfig)
 
-  def sendMessage(channel: String, message: String): Option[MessageSent] = session.flatMap { x ⇒
-    Option(x.findChannelByName(channel)).map { channel ⇒
-      x.sendMessage(channel, message)
+  def sendMessage(destination: Destination, message: SlackMessage): Option[MessageSent] = session.flatMap { x ⇒
+    destination match {
+      case SlackChannel(channel) ⇒ Option(x.findChannelByName(channel)).map { channel ⇒
+        x.sendMessage(channel, message.message, message.attachment.orNull)
+      }
+      case SlackUser(email) ⇒ Option(x.findUserByEmail(email)).map { user ⇒
+        x.sendMessageToUser(user, message.message, message.attachment.orNull)
+      }
     }
   }
 }
