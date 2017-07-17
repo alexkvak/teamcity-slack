709e2311d68e43e8cf3d6250168a5ba54ff67694
Alex Kvak <1052418+alexkvak@users.noreply.github.com>
2017-07-17 16:45:49 +0300

    slackNotify from strings removed

--- slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/Resources.scala
+++ slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/Resources.scala
@@ -10,10 +10,10 @@ object Resources {
   case class Page(url: String, view: String)
 
   lazy val buildPage = View("buildPage.jsp")
-  lazy val buildSettingList = Page("/app/slackNotify/buildSettingList.html", "buildSettingListPage.jsp")
-  lazy val buildSettingEdit = Page("/app/slackNotify/buildSettingEdit.html", "buildSettingEditPage.jsp")
-  lazy val buildSettingSave = Action("/app/slackNotify/buildSettingSave.html")
-  lazy val buildSettingDelete = Action("/app/slackNotify/buildSettingDelete.html")
-  lazy val configPage = Page("/app/slackNotify/config", "configPage.jsp")
+  lazy val buildSettingList = Page("/app/slackIntegration/buildSettingList.html", "buildSettingListPage.jsp")
+  lazy val buildSettingEdit = Page("/app/slackIntegration/buildSettingEdit.html", "buildSettingEditPage.jsp")
+  lazy val buildSettingSave = Action("/app/slackIntegration/buildSettingSave.html")
+  lazy val buildSettingDelete = Action("/app/slackIntegration/buildSettingDelete.html")
+  lazy val configPage = Page("/app/slackIntegration/config", "configPage.jsp")
   lazy val ajaxView = View("ajaxView.jsp")
 }
