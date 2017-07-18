f9911825f4fe68f532bcb7958dfbc70e05393921
Alex Kvak <1052418+alexkvak@users.noreply.github.com>
2017-07-18 16:20:52 +0300

    Old names removed

--- slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/Strings.scala
+++ slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/Strings.scala
@@ -1,6 +1,6 @@
 package com.fpd.teamcity.slack
 
 object Strings {
-  def label: String = "Slack Notifier"
-  def tabId: String = "SlackNotifier"
+  def label: String = "Slack"
+  def tabId: String = "Slack"
 }
