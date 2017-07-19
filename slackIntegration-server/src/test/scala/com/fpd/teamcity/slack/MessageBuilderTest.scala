a86295baa8605bb66323bf715e60cebb8d58be46
Alex Kvak <metalalisa@aks-studio.com>
2017-07-19 10:09:17 +0300

    Test fixed

--- slackIntegration-server/src/test/scala/com/fpd/teamcity/slack/MessageBuilderTest.scala
+++ slackIntegration-server/src/test/scala/com/fpd/teamcity/slack/MessageBuilderTest.scala
@@ -40,12 +40,14 @@ class MessageBuilderTest extends FlatSpec with MockFactory with Matchers {
     build.getBranch _ when() returns branch
     branch.getDisplayName _ when() returns "default"
     build.getBuildStatus _ when() returns Status.FAILURE
+    build.getContainingChanges _ when() returns mockChanges
 
     val viewResultsUrl = "http://localhost:8111/viewLog.html?buildId=2"
     messageBuilder(viewResultsUrl).compile(MessageBuilder.defaultMessage) shouldEqual SlackAttachment(
       s"""<$viewResultsUrl|Full name - 2>
         |Branch: default
         |Status: failed
+        |@nick1 @nick2
       """.stripMargin.trim, Status.FAILURE.getHtmlColor)
   }
 
@@ -82,19 +84,10 @@ class MessageBuilderTest extends FlatSpec with MockFactory with Matchers {
   "MessageBuilder.compile" should "compile template with mentions placeholders" in {
     implicit val build = stub[SBuild]
 
-    val vcsModification1 = stub[SVcsModification]
-    val vcsModification2 = stub[SVcsModification]
-    val user1 = stub[SUser]
-    val user2 = stub[SUser]
-    user1.getEmail _ when() returns "nick1"
-    user2.getEmail _ when() returns "nick2"
-    vcsModification1.getCommitters _ when() returns Set(user1).asJava
-    vcsModification2.getCommitters _ when() returns Set(user1, user2).asJava
-
     build.getFullName _ when() returns "Full name"
     build.getBuildNumber _ when() returns "2"
     build.getBuildStatus _ when() returns Status.FAILURE
-    build.getContainingChanges _ when() returns List(vcsModification1, vcsModification2).asJava
+    build.getContainingChanges _ when() returns mockChanges
 
     val messageTemplate = """{name}
                             |{mentions}
@@ -104,6 +97,19 @@ class MessageBuilderTest extends FlatSpec with MockFactory with Matchers {
       s"""Full name
         |@nick1 @nick2
       """.stripMargin.trim, Status.FAILURE.getHtmlColor)
+  }
+
+  private def mockChanges = {
+    val vcsModification1 = stub[SVcsModification]
+    val vcsModification2 = stub[SVcsModification]
+    val user1 = stub[SUser]
+    val user2 = stub[SUser]
+    user1.getEmail _ when() returns "nick1"
+    user2.getEmail _ when() returns "nick2"
+    vcsModification1.getCommitters _ when() returns Set(user1).asJava
+    vcsModification2.getCommitters _ when() returns Set(user1, user2).asJava
+
+    List(vcsModification1, vcsModification2).asJava
   }
 }
 
