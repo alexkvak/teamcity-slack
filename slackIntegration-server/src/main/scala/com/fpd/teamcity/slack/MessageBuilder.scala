aafec2a7c63687a9f993043de9aa941d7d1435db
Alex Kvak <1052418+alexkvak@users.noreply.github.com>
2017-08-18 15:52:25 +0300

    Project and build parameters support added. {artifacts} placeholder renamed to {allArtifactsDownloadUrl}. {artifactsRelUrl} placeholder added.

--- slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/MessageBuilder.scala
+++ slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/MessageBuilder.scala
@@ -2,23 +2,20 @@ package com.fpd.teamcity.slack
 
 import com.fpd.teamcity.slack.SlackGateway.SlackAttachment
 import jetbrains.buildServer.messages.Status
-import jetbrains.buildServer.serverSide.{SBuild, WebLinks}
+import jetbrains.buildServer.serverSide.{SBuild, ServerPaths, WebLinks}
 
 import scala.collection.JavaConverters._
 
-class MessageBuilder(
-                      build: SBuild,
-                      viewResultsUrl: (SBuild) ⇒ String,
-                      nickByEmail: (String) ⇒ Option[String],
-                      downloadArtifactsUrl: (SBuild) ⇒ String
-                    ) {
+class MessageBuilder(build: SBuild, context: MessageBuilderContext) {
   import MessageBuilder._
   import Helpers.Implicits._
 
   def compile(template: String): SlackAttachment = {
     def status = if (build.getBuildStatus.isSuccessful) "succeeded" else "failed"
 
-    def artifacts = s"<${downloadArtifactsUrl(build)}|Download all artifacts>"
+    def artifacts = s"<${context.getDownloadAllArtifactsUrl(build)}|Download all artifacts>"
+
+    def artifactsRelUrl = build.getArtifactsDirectory.getPath.stripPrefix(context.getArtifactsPath)
 
     def changes = build.getContainingChanges.asScala.take(5).map { change ⇒
       val name = change.getCommitters.asScala.headOption.map(_.getUsername).getOrElse("unknown")
@@ -28,18 +25,24 @@ class MessageBuilder(
     } mkString "\n"
 
     def mentions = if (build.getBuildStatus.isSuccessful) "" else {
-      build.committees.map(nickByEmail).collect { case Some(x) ⇒ s"@$x" }.mkString(" ")
+      build.committees.map(context.nickByEmail).collect { case Some(x) ⇒ s"@$x" }.mkString(" ")
     }
 
-    val text = """\{(\w+)\}""".r.replaceAllIn(template, m ⇒ m.group(1) match {
+    val text = """\{([\s\w._%]+)\}""".r.replaceAllIn(template, m ⇒ m.group(1) match {
       case "name" ⇒ build.getFullName
       case "number" ⇒ build.getBuildNumber
       case "branch" ⇒ build.getBranch.getDisplayName
       case "status" ⇒ status
       case "changes" ⇒ changes
-      case "artifacts" ⇒ artifacts
-      case "link" ⇒ viewResultsUrl(build)
+      case "allArtifactsDownloadUrl" ⇒ artifacts
+      case "artifactsRelUrl" ⇒ artifactsRelUrl
+      case "link" ⇒ context.getViewResultsUrl(build)
       case "mentions" ⇒ mentions
+      case x if x.startsWith("%") && x.endsWith("%") ⇒
+        context.getBuildParameter(build, x.substring(1, x.length - 1).trim) match {
+        case Some(value) ⇒ value
+        case _ ⇒ m.group(0)
+      }
       case _ ⇒ m.group(0)
     })
 
@@ -60,9 +63,22 @@ object MessageBuilder {
   private def statusColor(status: Status) = if (status == Status.NORMAL) statusNormalColor else status.getHtmlColor
 }
 
-class MessageBuilderFactory(webLinks: WebLinks, gateway: SlackGateway) {
-  private def nickByEmail(email: String): Option[String] = Option(gateway.session.get.findUserByEmail(email)).map(_.getUserName)
+class MessageBuilderFactory(webLinks: WebLinks, gateway: SlackGateway, paths: ServerPaths) {
+  private val context = new MessageBuilderContext(webLinks, gateway, paths)
 
-  def createForBuild(build: SBuild) = new MessageBuilder(build, webLinks.getViewResultsUrl, nickByEmail, webLinks.getDownloadAllArtefactsUrl)
+  def createForBuild(build: SBuild) = new MessageBuilder(build, context)
+}
+
+class MessageBuilderContext(webLinks: WebLinks, gateway: SlackGateway, paths: ServerPaths) {
+  def getViewResultsUrl: (SBuild) ⇒ String = webLinks.getViewResultsUrl
+
+  def getDownloadAllArtifactsUrl: (SBuild) ⇒ String = webLinks.getDownloadAllArtefactsUrl
+
+  def nickByEmail: (String) ⇒ Option[String] = email ⇒ Option(gateway.session.get.findUserByEmail(email)).map(_.getUserName)
+
+  def getArtifactsPath: String = paths.getArtifactsDirectory.getPath
+
+  def getBuildParameter: (SBuild, String) ⇒ Option[String] = (build, name) ⇒
+    Option(build.getBuildType.getParameter(name).getValue)
 }
 
