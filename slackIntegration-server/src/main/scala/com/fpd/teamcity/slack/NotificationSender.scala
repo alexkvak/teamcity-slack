8f0739a39bdb858c0a951c43c832bf6bda4113d5
Alex Kvak <metalalisa@gmail.com>
2018-01-17 15:36:39 +0300

    Personal build's notification should not send to channel #22

--- slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/NotificationSender.scala
+++ slackIntegration-server/src/main/scala/com/fpd/teamcity/slack/NotificationSender.scala
@@ -2,8 +2,10 @@ package com.fpd.teamcity.slack
 
 import com.fpd.teamcity.slack.ConfigManager.BuildSetting
 import com.fpd.teamcity.slack.ConfigManager.BuildSettingFlag.BuildSettingFlag
-import com.fpd.teamcity.slack.SlackGateway.{SlackChannel, SlackUser}
+import com.fpd.teamcity.slack.SlackGateway.{Destination, MessageSent, SlackChannel, SlackUser}
 import jetbrains.buildServer.serverSide.SBuild
+
+import scala.collection.mutable
 
 trait NotificationSender {
 
@@ -13,28 +15,41 @@ trait NotificationSender {
 
   import Helpers.Implicits._
 
-  def send(build: SBuild, flags: Set[BuildSettingFlag]): Unit = {
+  type SendResult = Map[String, Option[MessageSent]]
+
+  def send(build: SBuild, flags: Set[BuildSettingFlag]): SendResult = {
     val settings = prepareSettings(build, flags)
 
     lazy val emails = build.committees
     lazy val messageBuilder = messageBuilderFactory.createForBuild(build)
     lazy val sendPersonal = shouldSendPersonal(build)
 
-    settings.foreach { setting ⇒
+    settings.foldLeft(Map(): SendResult) { (acc, setting) ⇒
       val attachment = messageBuilder.compile(setting.messageTemplate, Some(setting))
-      if (setting.slackChannel.nonEmpty) {
-        gateway.sendMessage(SlackChannel(setting.slackChannel), attachment)
-      }
+      val destinations = mutable.Set.empty[Destination]
+      if (build.isPersonal) {
+        // If build is personal we need inform only build's owner if needed
+        val email = build.getOwner.getEmail
+        if (sendPersonal && email.length > 0) {
+          destinations += SlackUser(email)
+        }
+      } else {
+        if (setting.slackChannel.nonEmpty) {
+          destinations += SlackChannel(setting.slackChannel)
+        }
 
-      /**
-        * if build fails all committees should receive the message
-        * if personal notification explicitly enabled in build settings let's notify all committees
-        */
-      if (setting.notifyCommitter || sendPersonal) {
-        emails.foreach { email ⇒
-          gateway.sendMessage(SlackUser(email), attachment)
+        /**
+          * if build fails all committees should receive the message
+          * if personal notification explicitly enabled in build settings let's notify all committees
+          */
+        if (setting.notifyCommitter || sendPersonal) {
+          emails.foreach { email ⇒
+            destinations += SlackUser(email)
+          }
         }
       }
+
+      acc ++ destinations.map(x ⇒ x.toString → gateway.sendMessage(x, attachment)).toMap
     }
   }
 
